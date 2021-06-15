ed84f09a93a6ed91512193eabebcb224730bc789
Alexandre Destannes <alexandre.destannes@boxtal.com>
2021-06-15 21:04:02 +0200

    optimized item value service

--- src/app/slormancer/services/slormancer-item-value.service.ts
+++ src/app/slormancer/services/slormancer-item-value.service.ts
@@ -13,14 +13,6 @@ interface MinMax {
 @Injectable()
 export class SlormancerItemValueService {
 
-    private readonly RARITY_RATIO = {
-        'N': { min: 70, max: 100 },
-        'M': { min: 45, max: 65 },
-        'R': { min: 45, max: 65 },
-        'E': { min: 20, max: 40 },
-        'L': { min: 100, max: 100 }
-    }
-
     private readonly AFFIX_MIN_MAX: { [key: string]: { [key: string]: { [key: number]: MinMax }}} = {
         'N': {
             '': {
@@ -86,84 +78,58 @@ export class SlormancerItemValueService {
 
     constructor(private slormancerGameDataService : SlormancerGameDataService) { }
 
-    private getLevelPercentScore(item: GameEquippableItem): number {
-        return Math.max(1, Math.floor((item.level + 10) / 15));
+    private getLevelPercentScore(level: number): number {
+        return Math.max(1, Math.floor((level + 10) / 15));
     }
 
-    private getComputedBaseValue(item: GameEquippableItem, affix: GameAffix): number {
-        const stat = this.slormancerGameDataService.getGameStatData(affix);
-        let result = affix.value;
-
-        if (stat !== null) {
-            if (stat.PERCENT === '%') {
-                result = this.getLevelPercentScore(item) * stat.SCORE * 20;
-            } else if (stat.PERCENT === '') {
-                result = stat.SCORE * (100 + (item.level * 30)) / 100;
-            }
-        }
-
-        return result;
+    private getComputedBaseValue(level: number, score: number, percent: boolean): number {
+        return percent
+            ? this.getLevelPercentScore(level) * score * 20
+            : score * (100 + (level * 30)) / 100;
     }
 
-    private roundValue(value: number, affix: GameAffix): number {
-        const stat = this.slormancerGameDataService.getGameStatData(affix);
+    private roundValue(value: number, score: number, percent: boolean): number {
         let result = value;
 
-        if (stat !== null) {
-            if (stat.PERCENT === '%') {
-                if (stat.SCORE < 5) {
-                    result = bankerRound(value * 10) / 1000;
-                } else {
-                    result = bankerRound(value / 50) / 2;
-                }
-            } else if (stat.PERCENT === '') {
-                result = Math.max(1, bankerRound(value));
+        if (percent) {
+            if (score < 5) {
+                result = bankerRound(value * 10) / 1000;
+            } else {
+                result = bankerRound(value / 50) / 2;
             }
+        } else {
+            result = Math.max(1, bankerRound(value));
         }
 
         return result;
     }
 
-    private getRarityRatio(affix: GameAffix): MinMax {     
-        return this.RARITY_RATIO[affix.rarity];
-    }
+    private getValueRatio(level: number, value: number, percent: boolean): number {
+        const levelScore = this.getLevelPercentScore(level);
+        let ratio = value;
 
-    private getValueRatio(item: GameEquippableItem, affix: GameAffix): number {
-        const stat = this.slormancerGameDataService.getGameStatData(affix);
-        const levelScore = this.getLevelPercentScore(item);
-        let ratio = affix.value;
-
-        if (stat !== null && stat.PERCENT === '%') {
+        if (percent) {
             ratio = ratio * 5 / levelScore;
         }
 
         return ratio;
     }
 
-    private computeAffixValueRange(item: GameEquippableItem, affix: GameAffix): MinMax {
-        const value = this.getComputedBaseValue(item, affix);
-        const ratio = this.getRarityRatio(affix);
-        const reinforcment = 100 + (15 * item.reinforcment);
-
-        return {
-            min: this.roundValue(value * reinforcment * ratio.min / (100 * 100), affix),
-            max: this.roundValue(value * reinforcment * ratio.max / (100 * 100), affix)
-        };
+    private getReinforcmentratio(reinforcment: number): number {
+        return 100 + (15 * reinforcment);
     }
 
-    private computeAffixValue(item: GameEquippableItem, affix: GameAffix): number {
-        const value = this.getComputedBaseValue(item, affix);
-        const reinforcment = 100 + (15 * item.reinforcment);
-        const ratio = this.getValueRatio(item, affix);
+    private computeAffixValue(level: number, reinforcment: number, score: number, value: number, percent: boolean): number {
+        const baseValue = this.getComputedBaseValue(level, score, percent);
+        const reinforcmentRatio = this.getReinforcmentratio(reinforcment);
+        const valueRatio = this.getValueRatio(level, value, percent);
 
-        return this.roundValue(value * reinforcment * ratio / (100 * 100), affix);
+        return this.roundValue(baseValue * reinforcmentRatio * valueRatio / (100 * 100), score, percent);
     }
 
     private getAffixMinMax(rarity: GameRarity, percent: string, levelScore: number): MinMax | null {
         let minMax: MinMax | null = null;
         
-        console.log(rarity, percent, levelScore, )
-
         const rarityMinmax = this.AFFIX_MIN_MAX[rarity];
         if (rarityMinmax) {
             const percentMinMax = rarityMinmax[percent];
@@ -179,37 +145,15 @@ export class SlormancerItemValueService {
     public getAffixValues(item: GameEquippableItem, affix: GameAffix): { [ key: number]: number } {
         const stat = this.slormancerGameDataService.getGameStatData(affix);
         let values: { [key: number]: number } = { [affix.value] : 0 };
-        const minMax = this.computeAffixValueRange(item, affix);
-        const levelScore = this.getLevelPercentScore(item);
+        const levelScore = this.getLevelPercentScore(item.level);
 
         if (stat !== null) {
-            for (let i = 1 ; i <= 100 ; i++) {
-                const tmpAffixe = { ...affix, value: i };
-                const tmpValue = this.computeAffixValue(item, tmpAffixe);
-    
-                if (tmpValue >= minMax.min && tmpValue <= minMax.max) {
-                    values[i] = tmpValue;
-                }
-            }
-            const keys = Object.keys(values).map(k => parseInt(k));
-            const min = keys[0];
-            const max = keys[keys.length - 1];
-            const minValues = keys.filter(k => min && values[k] === values[min]);
-            const maxValues = keys.filter(k => max && values[k] === values[max]);
-    
             const range = this.getAffixMinMax(affix.rarity, stat.PERCENT, levelScore);
-            
+
             if (range !== null) {
-                if (minValues.indexOf(range.min) === -1 || maxValues.indexOf(range.max) === -1) {
-                    console.error('Invalid range values for ', affix.rarity, '-', stat.PERCENT, '-', levelScore, ' : ', range.min, ' - ', range.max);
-                    console.log('Possible min-max : ', minValues.join(','), ' - ', maxValues.join(','));
-                    console.log('Computed values : ', values);
-                } else {
-                    values = {};
-                    for (let value of list(range.min, range.max)) {
-                        const tmpAffixe = { ...affix, value };
-                        values[value] = this.computeAffixValue(item, tmpAffixe);
-                    }
+                values = {};
+                for (let value of list(range.min, range.max)) {
+                    values[value] = this.computeAffixValue(item.level, item.reinforcment, stat.SCORE, value, stat.PERCENT === '%');
                 }
             }
         }
