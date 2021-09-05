0c1d408332ec8ae00532452ba0f385b059f3d347
Alexandre Destannes <alexandre.destannes@boxtal.com>
2021-09-05 15:41:25 +0200

    Added idle images

--- src/app/modules/build/component/inventory/inventory.component.ts
+++ src/app/modules/build/component/inventory/inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { takeUntil } from 'rxjs/operators';
 
 import {
@@ -15,6 +15,20 @@ import { Character } from '../../../slormancer/model/character';
 export class InventoryComponent extends AbstractUnsubscribeComponent implements OnInit {
 
     public character: Character | null = null;
+
+    public idle: number = 0;
+
+    @HostListener('window:keydown', ['$event'])
+    spaceEvent(event: any) {
+        if (event.keyCode == 81) { // left
+            this.idle--;
+        } else if (event.keyCode == 68) {
+            this.idle++;
+        }
+
+        this.idle = (this.idle + 10) % 10;
+
+    }
 
     constructor(private plannerService: PlannerService) {
         super();
