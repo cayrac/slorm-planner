6ffc4045935678a5d12d1982028fa5a9348bad22
Alexandre Destannes <alexandre.destannes@boxtal.com>
2021-10-05 23:56:26 +0200

    Minor effect value service optimization

--- src/app/modules/slormancer/services/content/slormancer-effect-value.service.ts
+++ src/app/modules/slormancer/services/content/slormancer-effect-value.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { AbstractEffectValue } from '../../model/content/effect-value';
 import { EffectValueUpgradeType } from '../../model/content/enum/effect-value-upgrade-type';
-import { round } from '../../util/math.util';
 import { isEffectValueSynergy, isEffectValueVariable } from '../../util/utils';
 
 @Injectable()
@@ -15,40 +14,19 @@ export class SlormancerEffectValueService {
     }
 
     public updateEffectValue(effectValue: AbstractEffectValue, upgradeMultiplier: number): AbstractEffectValue {
-        const displayUpgradeMultiplier = Math.max(upgradeMultiplier, 0);
         if (isEffectValueSynergy(effectValue) || isEffectValueVariable(effectValue)) {
-            let value = effectValue.baseValue * 1000;
-            let displayValue = effectValue.baseValue * 1000;
+            let value = effectValue.baseValue;
             if (effectValue.upgradeType === EffectValueUpgradeType.Every3) {
-                value = Math.max(value, 1000 * effectValue.upgrade * Math.ceil((upgradeMultiplier + 1) / 3));
-                // 1 => 2
-                // 2 => 2
-                // 3 => 2
-                // 4 => 2
-                // 5 => 2
-                // 6 => 3
-                // 7 => 3
-                // 8 => 3
-                // 9 => 4
-                // 10 => 4
-                // 11 => 4
-                // 12 => 5
-                // 13 => 5
-                // 14 => 5
-                // 15 => 6
-                displayValue = value;
+                value = Math.max(value, effectValue.upgrade * Math.ceil((upgradeMultiplier + 1) / 3));
             } else {
-                value += 1000 * effectValue.upgrade * upgradeMultiplier;
-                displayValue += 1000 * effectValue.upgrade * displayUpgradeMultiplier;
+                value += effectValue.upgrade * upgradeMultiplier;
             }
-            effectValue.value = value / 1000;
 
             if (isEffectValueVariable(effectValue)) {
-                effectValue.upgradedValue = effectValue.value;
+                effectValue.upgradedValue = value;
+            } else {
+                effectValue.value = value;
             }
-
-            // TODO the round here is necessary on the displayValue to keep the real hidden value
-            effectValue.displayValue = round(displayValue / 1000, 3);
         }
 
         return effectValue;
